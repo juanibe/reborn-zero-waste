43b9191c0f77c94c0c4aa1a6443794117a7144c6
Camille Chapleau <camille.chapleau@hotmail.fr>
2019-03-28 20:43:11 +0100

    changes

--- client/src/pages/Designer-profile-public.js
+++ client/src/pages/Designer-profile-public.js
@@ -2,53 +2,45 @@ import React, { Component } from "react";
 import "../App.css";
 import { Card, Container, Row, Col } from "react-bootstrap";
 
-//import AuthService from './auth-service';
-
 class DesignerPublic extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     someData: "",
-  //     someData2: ""
-  //   };
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      someData: "",
+      someData2: ""
+    };
+  }
 
   render() {
     return (
-      <div className="container">
-        <h3>Get in touch with designers right now ! </h3>
-        <div className="row">
-          <Container>
-            <Row>
-              <Col>
-                <Card style={{ width: "18rem" }}>
-                  <div className="card">
-                    <img className="card-img-top" alt="designer" />
-                    {/* <div className="card-body"> */}
-                    {/* <h5 className="card-name">{props.name}</h5> */}
-                    {/* <p className="card-text">{props.text}</p> */}
-                    {/* </div> */}
-                    {/* <div className="card-body">{props.signUpLink}</div> */}
-                  </div>
-                </Card>
-              </Col>
+      <Container>
+        <h2>Get in touch with designers right now ! </h2>
+        <Row>
+          <Col>
+            <Card style={{ width: "18rem" }}>
+              <div className="card">
+                <img className="card-img-top" alt="designer" />
+                {/* <div className="card-body"> */}
+                {/* <h5 className="card-name">{props.name}</h5> */}
+                {/* <div className="contact-button">{props.}</div> */}
+                {/* </div> */}
+              </div>
+            </Card>
+          </Col>
 
-              <Col>
-                <Card style={{ width: "18rem" }}>
-                  <div className="card">
-                    <img className="card-img-top" alt="designer" />
-                    {/* <div className="card-body"> */}
-                    {/* <h5 className="card-name">{props.name}</h5> */}
-                    {/* <p className="card-text">{props.text}</p> */}
-                    {/* </div> */}
-                    {/* <div className="card-body">{props.signUpLink}</div> */}
-                  </div>
-                </Card>
-              </Col>
-            </Row>
-          </Container>
-        </div>
-      </div>
+          <Col>
+            <Card style={{ width: "18rem" }}>
+              <div className="card">
+                <img className="card-img-top" alt="designer" />
+                {/* <div className="card-body"> */}
+                {/* <h5 className="card-name">{props.name}</h5> */}
+                {/* <div className="contact-button">{props.}</div> */}
+                {/* </div> */}
+              </div>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
--- server/routes/auth-routes.js
+++ server/routes/auth-routes.js
@@ -1,129 +1,131 @@
-const express = require('express');
+const express = require("express");
 const authRoutes = express.Router();
-const passport = require('passport');
-const bcrypt = require('bcryptjs');
+const passport = require("passport");
+const bcrypt = require("bcryptjs");
 
 // require the user model !!!!
-const User = require('../models/User');
-
+const User = require("../models/User");
 
 // ---------- SIGNUP --------- //
 
-authRoutes.get('/signup', (req, res, next) => {
-    res.status(200).json({ message: 'Everything is ok...so far' });
-})
-
-authRoutes.post('/signup', (req, res, next) => {
-    // User.create({
-    //     username: req.body.username,
-    //     password: req.body.password
-    // }).then(response => {res.json(response)}).catch(err => {err.json(err)});
-    const username = req.body.username;
-    const password = req.body.password;
-    const email = req.body.email;
-    const type = req.body.type;
-
-    if (!username || !password) {
-        res.status(400).json({ message: 'Provide username and password' });
-        return;
-    }
-
-    if (password.length < 7) {
-        res.status(400).json({ message: 'Please make your password at least 8 characters long for security purposes.' });
-        return;
-    }
-
-    User.findOne({ username }, (err, foundUser) => {
-
-        if (err) {
-            res.status(500).json({ message: "Username check went bad." });
-            return;
-        }
-
-        if (foundUser) {
-            res.status(400).json({ message: 'Username taken. Choose another one.' });
-            return;
-        }
-
-        const salt = bcrypt.genSaltSync(10);
-        const hashPass = bcrypt.hashSync(password, salt);
-
-        const aNewUser = new User({
-            username: username,
-            password: hashPass,
-            email: email,
-            type: type
-        });
-
-        aNewUser.save(err => {
-            if (err) {
-                res.status(400).json({ message: 'Saving user to database went wrong.' });
-                return;
-            }
-
-            // Automatically log in user after sign up
-            // .login() here is actually predefined passport method
-            req.login(aNewUser, (err) => {
-
-                if (err) {
-                    res.status(500).json({ message: 'Login after signup went bad.' });
-                    return;
-                }
-
-                // Send the user's information to the frontend
-                // We can use also: res.status(200).json(req.user);
-                res.status(200).json(aNewUser);
-            });
-        });
-    });
+authRoutes.get("/signup", (req, res, next) => {
+  res.status(200).json({ message: "Everything is ok...so far" });
 });
 
+authRoutes.post("/signup/manufacturer", (req, res, next) => {
+  // User.create({
+  //     username: req.body.username,
+  //     password: req.body.password
+  // }).then(response => {res.json(response)}).catch(err => {err.json(err)});
+  const username = req.body.username;
+  const password = req.body.password;
+  const email = req.body.email;
+  const type = req.body.type;
+
+  if (!username || !password) {
+    res.status(400).json({ message: "Provide username and password" });
+    return;
+  }
+
+  if (password.length < 7) {
+    res.status(400).json({
+      message:
+        "Please make your password at least 8 characters long for security purposes."
+    });
+    return;
+  }
+
+  User.findOne({ username }, (err, foundUser) => {
+    if (err) {
+      res.status(500).json({ message: "Username check went bad." });
+      return;
+    }
+
+    if (foundUser) {
+      res.status(400).json({ message: "Username taken. Choose another one." });
+      return;
+    }
+
+    const salt = bcrypt.genSaltSync(10);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    const aNewUser = new User({
+      username: username,
+      password: hashPass,
+      email: email,
+      type: type
+    });
+
+    aNewUser.save(err => {
+      if (err) {
+        res
+          .status(400)
+          .json({ message: "Saving user to database went wrong." });
+        return;
+      }
+
+      // Automatically log in user after sign up
+      // .login() here is actually predefined passport method
+      req.login(aNewUser, err => {
+        if (err) {
+          res.status(500).json({ message: "Login after signup went bad." });
+          return;
+        }
+
+        // Send the user's information to the frontend
+        // We can use also: res.status(200).json(req.user);
+        res.status(200).json(aNewUser);
+      });
+    });
+  });
+});
 
 // ---------- LOGIN ---------- //
 
-authRoutes.post('/login', (req, res, next) => {
-    console.log("AT LOGIN")
+authRoutes.post("/login", (req, res, next) => {
+  console.log("AT LOGIN");
 
-    passport.authenticate('local', (err, theUser, failureDetails) => {
-        if (err) {
-            res.status(500).json({ message: 'Something went wrong authenticating user' });
-            return;
-        }
-        console.log("USER", theUser)
-        if (!theUser) {
-            // "failureDetails" contains the error messages
-            // from our logic in "LocalStrategy" { message: '...' }.
-            res.status(401).json(failureDetails);
-            return;
-        }
-
-        // save user in session
-        req.login(theUser, (err) => {
-            if (err) {
-                res.status(500).json({ message: 'Session save went bad.' });
-                return;
-            }
-
-            // We are now logged in (that's why we can also send req.user)
-            res.status(200).json(theUser);
-        });
-    })(req, res, next);
-});
-
-authRoutes.post('/logout', (req, res, next) => {
-    // req.logout() is defined by passport
-    req.logout();
-    res.status(200).json({ message: 'Log out success!' });
-});
-
-
-authRoutes.get('/loggedin', (req, res, next) => {
-    // req.isAuthenticated() is defined by passport
-    if (req.isAuthenticated()) {
-        res.status(200).json(req.user);
-        return;
+  passport.authenticate("local", (err, theUser, failureDetails) => {
+    if (err) {
+      res
+        .status(500)
+        .json({ message: "Something went wrong authenticating user" });
+      return;
     }
-    // res.status(403).json({ message: 'Unauthorized' });
+    console.log("USER", theUser);
+    if (!theUser) {
+      // "failureDetails" contains the error messages
+      // from our logic in "LocalStrategy" { message: '...' }.
+      res.status(401).json(failureDetails);
+      return;
+    }
+
+    // save user in session
+    req.login(theUser, err => {
+      if (err) {
+        res.status(500).json({ message: "Session save went bad." });
+        return;
+      }
+
+      // We are now logged in (that's why we can also send req.user)
+      res.status(200).json(theUser);
+    });
+  })(req, res, next);
+});
+
+authRoutes.post("/logout", (req, res, next) => {
+  // req.logout() is defined by passport
+  req.logout();
+  res.status(200).json({ message: "Log out success!" });
+});
+
+authRoutes.get("/loggedin", (req, res, next) => {
+  // req.isAuthenticated() is defined by passport
+  if (req.isAuthenticated()) {
+    res.status(200).json(req.user);
+    return;
+  }
+  // res.status(403).json({ message: 'Unauthorized' });
 });
 
 module.exports = authRoutes;
